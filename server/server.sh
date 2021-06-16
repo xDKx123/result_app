978f2df461dfcd8fe80d949acf654ab76e8f0b34
xDKx123 <38501298+xDKx123@users.noreply.github.com>
2021-06-16 15:07:11 +0200

    Added docker files

---
+++ server/server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Welcome
+echo 'Server start script initialized...'
+
+# Set the port
+PORT=4040
+
+# Kill anything that is already running on that port
+echo 'Cleaning port' $PORT '...'
+
+# Change directories to the release folder
+cd build/web/
+
+# Start the server
+echo 'Starting server on port' $PORT '...'
+python3 -m http.server $PORT
+
+# Exit
+echo 'Server exited...'
